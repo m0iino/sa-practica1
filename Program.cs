5cb11199197869988638dba7fffc90d3f9c509df
Cristian <moino.cristian@gmail.com>
2020-08-04 21:45:50 -0600

    Program en c#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&id=1&api=hal";
+            var json = new WebClient().DownloadString(url);
+            dynamic m = JsonConvert.DeserializeObject(json);
+            Console.WriteLine(json);
+            
+        }
+    }
+}
